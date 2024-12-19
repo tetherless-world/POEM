56a2866ac7910afdb97e8b0546bddfbeae2c0f57
Kelsey <kmrookie16@gmail.com>
2024-12-19 15:04:29 -0500

    Fixed source directory for .ttl files

--- swj_submission/get_all_individuals.py
+++ swj_submission/get_all_individuals.py
@@ -1,28 +1,25 @@
-from rdflib import Graph, Literal, Namespace, BNode
+from rdflib import Graph, Literal, Namespace, BNode, URIRef
 from rdflib.namespace import RDF, XSD
 import os
 
 def main():
     g = Graph()
 
-    DC = Namespace("http://purl.org/dc/terms/")
-    g.bind("dc", DC, replace=True)
-
-    #SIO = Namespace("http://semanticscience.org/ontology/sio.owl")
-    #g.bind("sio", SIO, replace=True)
-
-    files = os.listdir('./individuals')#('./swj_submission/individuals')
+    files = os.listdir('./individuals/')
     for file in files:
         if file.endswith('.ttl') and file != 'individuals_full.ttl':
             print("parsing {}".format(file))
-            g.parse(f"./individuals/{file}")
+            g.parse(f'individuals/{file}')
             print("parsed {}".format(file))
 
+    test = BNode()
     ont = BNode()
     OWL = Namespace("http://www.w3.org/2002/07/owl#")
+    uri = URIRef("http://purl.org/twc/poem")
     g.bind("owl", OWL)
+    g.add((test, RDF.type, ont))
     g.add((ont, RDF.type, OWL.Ontology))
-    g.add((ont, OWL.imports, Literal("https://raw.githubusercontent.com/tetherless-world/POEM/evidence-modeling/POEM.rdf", datatype=XSD.string)))
+    g.add((ont, OWL.imports, uri))
 
     g.serialize(destination='./individuals_full.ttl')
 
