8df9c4c28f5b43d6c36236d8baab47b07a2db12b
Henrique Santos <oliveh@rpi.edu>
2025-03-05 21:15:41 -0500

    read from dist

--- poem-demo/app/utils/POEMModel.java
+++ poem-demo/app/utils/POEMModel.java
@@ -6,18 +6,18 @@ import org.apache.jena.rdf.model.ModelFactory;
 public class POEMModel {
     public static Model getModel() {
         Model model = ModelFactory.createDefaultModel();
-        model.read("/Users/hansi/git/POEM/poem-demo/public/data/activities.ttl");
-        model.read("/Users/hansi/git/POEM/poem-demo/public/data/codebooks.ttl");
-        model.read("/Users/hansi/git/POEM/poem-demo/public/data/experiences.ttl");
-        model.read("/Users/hansi/git/POEM/poem-demo/public/data/informants.ttl");
-        model.read("/Users/hansi/git/POEM/poem-demo/public/data/instrumentItemMap.ttl");
-        model.read("/Users/hansi/git/POEM/poem-demo/public/data/instruments.ttl");
-        model.read("/Users/hansi/git/POEM/poem-demo/public/data/items.ttl");
-        model.read("/Users/hansi/git/POEM/poem-demo/public/data/itemStemConcepts.ttl");
-        model.read("/Users/hansi/git/POEM/poem-demo/public/data/itemStems.ttl");
-        model.read("/Users/hansi/git/POEM/poem-demo/public/data/responseOptions.ttl");
-        model.read("/Users/hansi/git/POEM/poem-demo/public/data/scaleItemConceptMap.ttl");
-        model.read("/Users/hansi/git/POEM/poem-demo/public/data/scales.ttl");
+        model.read("./data/activities.ttl");
+        model.read("./data/codebooks.ttl");
+        model.read("./data/experiences.ttl");
+        model.read("./data/informants.ttl");
+        model.read("./data/instrumentItemMap.ttl");
+        model.read("./data/instruments.ttl");
+        model.read("./data/items.ttl");
+        model.read("./data/itemStemConcepts.ttl");
+        model.read("./data/itemStems.ttl");
+        model.read("./data/responseOptions.ttl");
+        model.read("./data/scaleItemConceptMap.ttl");
+        model.read("./data/scales.ttl");
         return model;
     }
 }
