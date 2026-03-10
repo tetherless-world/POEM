6cfd5d3d87ddfa9e7b7c63244b06ff50908168e4
legffy <mrrst3@gmail.com>
2026-03-10 11:11:33 -0400

    fix: docker deployment

--- browser/backend/main.py
+++ browser/backend/main.py
@@ -20,7 +20,7 @@ app: FastAPI = FastAPI(lifespan = lifespan)
 app.include_router(router)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173","http://127.0.0.1:8080", "http://localhost:8080"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
