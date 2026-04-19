c0b385f1188556e8e8251c90bea08a7af6a26aa7
legffy <mrrst3@gmail.com>
2026-04-19 11:43:14 -0400

    explore sections

--- browser/frontend/src/components/instrumentSection.tsx
+++ browser/frontend/src/components/instrumentSection.tsx
@@ -1,5 +1,5 @@
 import type { LucideIcon } from "lucide-react";
-
+import { useNavigate } from "react-router-dom";
 export type instrumentProps = {
   name: string;
   description: string;
@@ -12,6 +12,7 @@ export default function InstrumentSection({
   count,
   Icon,
 }: instrumentProps) {
+  const navigate = useNavigate();
   return (
     <div className="border-2 shadow-xl hover:shadow-2xl space-y-5">
       <div className="h-3 bg-amber-500 ">
@@ -23,7 +24,7 @@ export default function InstrumentSection({
         </div>
         <h3 className="text-xl">{name}</h3>
         <p>{description}</p>
-        <button>Explore </button>
+        <button className = "border-2 bg-amber-500 hover:bg-amber-600 p-2 mt-4"onClick={()=> navigate(`/search?q=${name}`)}>Explore </button>
       </div>
     </div>
   );
